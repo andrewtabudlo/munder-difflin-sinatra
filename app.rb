9766c25c8e01a67b05f35184549228737844b652
andrewtabudlo <andrew.tabudlo@gmail.com>
2018-04-14 18:42:02 -0400

    fix routes to staff

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'chuck_norris'
+require_relative './staff.rb'
 
 get '/' do
   erb :index
@@ -14,10 +15,28 @@ get '/staff' do
   erb :staff
 end
 
-get '/staff/:id' do
-  joke = ChuckNorris::JokeFinder.get_joke
-  michael = Staff_Member.new('michael', './public/images/michael.jpg', joke)
-  if params[:id] == 'michael'
 
+joke = ChuckNorris::JokeFinder.get_joke
+chuck_joke = joke.joke
+@@michael = Staff_Member.new('Michael', '/images/michael.jpg', 'Regional Manager', chuck_joke)
+@@asian_jim = Staff_Member.new('Asian Jim', '/images/asian_jim.jpg', 'Accountant', chuck_joke)
+@@other_stephanie = Staff_Member.new('Other Stephanie', '/images/other_stephanie.jpg', 'Whatever Original Stephanie did', chuck_joke)
+@@wizard = Staff_Member.new('Wizard', '/images/wizard.jpg', 'In house magician', chuck_joke)
+
+get '/staff/:name' do
+  if params[:name] == 'michael'
+    @staff = @@michael
+    erb :staffpage
+  elsif params[:name] == 'asian-jim'
+    @staff = @@asian_jim
+    erb :staffpage
+  elsif params[:name] == 'other-stephanie'
+    @staff = @@other_stephanie
+    erb :staffpage
+  elsif params[:name] == 'wizard'
+    @staff = @@wizard
+    erb :staffpage
+  else
+    redirect '/error/'
   end
 end
--- staff.rb
+++ staff.rb
@@ -1,9 +1,10 @@
 class Staff_Member
-  attr_accessor :name, :pic, :quote
+  attr_accessor :name, :pic, :position, :quote
 
-  def initialize(name, pic, quote)
+  def initialize(name, pic, position, quote)
     @name = name
     @pic = pic
+    @position = position
     @quote = quote
   end
 end
